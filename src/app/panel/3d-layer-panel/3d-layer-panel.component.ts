d23bc708cc283d7c2e88cde6e1533a613cd086f5
Gianou <david.gianadda@students.hevs.ch>
2026-02-04 10:29:11 +0100

    feat: conditional rendering of 3d control panel, based on map library

--- src/app/panel/3d-layer-panel/3d-layer-panel.component.ts
+++ src/app/panel/3d-layer-panel/3d-layer-panel.component.ts
@@ -2,8 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { MapStateService } from '../../services/map-state.service';
 import { Leaflet3DPanelComponent } from './leaflet-3d-panel/leaflet-3d-panel.component';
-import { OpenLayers3DPanelComponent } from './openlayers-3d-panel/openlayers-3d-panel.component';
 import { MapLibre3DPanelComponent } from './maplibre-3d-panel/maplibre-3d-panel.component';
+import { OpenLayers3DPanelComponent } from './openlayers-3d-panel/openlayers-3d-panel.component';
 
 @Component({
   selector: 'app-3d-layer-panel',
